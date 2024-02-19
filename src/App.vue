99dbf2f8b796d7723b39c795a653466627baec47
SystemsFailure <allistirking422@gmail.com>
2024-02-19 23:31:47 +0300

    upd -01

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,74 @@
 /* eslint-disable */
 <template>
-    <Swiper ref="swiperRef" :options="swiperOptions" @swiper="setThumbsSwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-    </Swiper>
-    <div style="width: 100px; height: 100px; background-color: red;" @click="triger_"></div>
+    <div ref="mainRef" class="main_class">
+        <swiper 
+            ref="swiperRef" 
+            @swiper="setSwiper" 
+            direction="vertical"
+            effect="fade"
+            class="swiper swiper-fade"
+        >   
+            <swiper-slide class="swiper-slide">Slide 1</swiper-slide>
+            <swiper-slide class="swiper-slide">Slide 2</swiper-slide>
+            <swiper-slide class="swiper-slide">Slide 3</swiper-slide>
+            <swiper-slide class="swiper-slide">Slide 4</swiper-slide>
+            <swiper-slide class="swiper-slide">Slide 5</swiper-slide>
+        </swiper>
+    </div>
 </template>
 
 <script setup>
 import { Swiper, SwiperSlide } from "swiper/vue";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import "swiper/css";
-const swiperOptions = {
-    loop: true,
-    autoplay: {
-        delay: 5000,
-    },
-}
+import "swiper/css/effect-fade"
 
 const swiperRef = ref(null);
+const mainRef = ref(null);
 
-const setThumbsSwiper = (swiper) => {
+const setSwiper = (swiper) => {
     swiperRef.value = swiper
 }
 
-function triger_() {
-    console.log("swiperRef.value", swiperRef.value)
+function next() {
     swiperRef.value.slideNext();
 }
 
+function prev() {
+    swiperRef.value.slidePrev();
+}
+
+function debounce(func, delay) {
+    let timer;
+    return function() {
+        const context = this;
+        const args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.apply(context, args);
+        }, delay);
+    }
+}
+
+const debouncedNext = debounce(next, 200); // Ограничение на 2 секунды для перелистывания вперед
+const debouncedPrev = debounce(prev, 200); // Ограничение на 2 секунды для перелистывания назад
+
+onMounted(() => {
+    mainRef.value.addEventListener('wheel', function(event) {
+        if (event.deltaY > 0) {
+            console.log('Событие колеса мыши вниз');
+            debouncedNext();
+        } else if (event.deltaY < 0) {
+            console.log('Событие колеса мыши вверх');
+            debouncedPrev();
+        }
+    });
+})
+
+
 </script>
 
-<style>
+<style lang="css">
 * {
     padding: 0;
     margin: 0;
@@ -41,4 +78,29 @@ function triger_() {
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
 }
+
+.main_class {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(0, 0, 0);
+    padding: 0;
+    margin: 0;
+    color: white;
+}
+
+.swiper {
+    height: 100vh;
+}
+
+.swiper-slide {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+
 </style>
