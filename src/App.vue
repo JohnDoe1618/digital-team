5c6720df5d10f620e292282bd63240a2a7c6f975
SystemsFailure <allistirking422@gmail.com>
2024-02-22 22:06:47 +0300

    deleted swiper && added gsap

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,13 @@
 /* eslint-disable */
 <template>
     <div ref="mainRef" class="main_class">
-        <div class="bg-blur-img"></div>
-        <swiper 
-        ref="swiperRef" 
-        @swiper="setSwiper" 
-        direction="vertical"
-        effect="fade"
-        class="swiper swiper-fade"
-        >   
-                <swiper-slide class="swiper-slide" v-for="(slide, index) in slides" :key="index">
-                    {{ slide }}
-                </swiper-slide>
-            </swiper>
+        <div class="bg-blur-img">
+            
         </div>
+    </div>
 </template>
 
 <script setup>
-import { Swiper, SwiperSlide } from "swiper/vue";
-import { ref, onMounted } from "vue";
-import "swiper/css";
-import "swiper/css/effect-fade"
-
-const swiperRef = ref(null);
-const mainRef = ref(null);
-const slides = ["About team", "Team", "Our products", "More", "Deal"]
-const setSwiper = (swiper) => {
-    swiperRef.value = swiper
-}
-
-function next() {
-    swiperRef.value.slideNext();
-}
-
-function prev() {
-    swiperRef.value.slidePrev();
-}
-
-// тупо стандартная дебоунс функция
-// function debounce(func, delay) {
-//     let timer;
-//     return function() {
-//         const context = this;
-//         const args = arguments;
-//         clearTimeout(timer);
-//         timer = setTimeout(() => {
-//             func.apply(context, args);
-//         }, delay);
-//     }
-// }
-
-// const debouncedNext = debounce(next, 200);
-// const debouncedPrev = debounce(prev, 200);
-
-onMounted(() => {
-    mainRef.value.addEventListener('wheel', function(event) {
-        if (event.deltaY > 0) {
-            console.log('Событие колеса мыши вниз');
-            // debouncedNext();
-            next()
-        } else if (event.deltaY < 0) {
-            console.log('Событие колеса мыши вверх');
-            // debouncedPrev();
-            prev()
-        }
-    });
-})
-
 
 </script>
 
@@ -79,59 +20,6 @@ onMounted(() => {
 
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
-}
-
-.main_class {
-    position: relative;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgb(0, 0, 0);
-    color: white;
-    font-size: 30px;
-    padding: 20px;
-}
-
-.bg-blur-img {
-    position: absolute;
-    z-index: 1;
-    width: 100%;
-    height: 100%;
-    background-image: url(../src/assets/01.jpg);  
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-    filter: blur(30px); /* Размытие картинки */
-
-}
-
-.swiper {
-    height: 95vh;
-    border-radius: 15px;
-}
-
-.swiper-slide {
-    height: 100%;
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    background-color: black;
-    opacity: 0.8;
-    backdrop-filter: blur(200px);
-    z-index: 2;
-    border-radius: 15px;
-
-}
-
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter, .fade-leave-to {
-  opacity: 0;
 }
 
 </style>
