e7e1d1204ea96ef2c904670cb94b982e0d20205d
AveryOn <lnkonion2@gmail.com>
2024-02-24 01:42:31 +0300

    last-commit

--- src/main.js
+++ src/main.js
@@ -3,9 +3,22 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify';
+import * as icons from '@mdi/font/css/materialdesignicons.css';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons,
+})
 
 const app = createApp(App);
 
 app
     .use(router)
+    .use(vuetify)
     .mount('#app');
